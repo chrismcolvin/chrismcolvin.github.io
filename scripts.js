a33457f3f0f0571a579599aa12dc74be59cd3d6c
Chris Colvin <chrismcolvin@hotmail.com>
2021-01-16 00:11:22 -0500

    dark theme

--- scripts.js
+++ scripts.js
@@ -119,4 +119,21 @@ document.addEventListener("DOMContentLoaded", function(){
             })
         }
     }
+
+    // Select the button
+    const btn = document.querySelector('.btn-toggle');
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // Listen for a click on the button 
+    btn.addEventListener("click", function() {
+        // If the OS is set to dark mode...
+        if (prefersDarkScheme.matches) {
+        // ...then apply the .light-theme class to override those styles
+        document.body.classList.toggle("light-theme");
+        // Otherwise...
+        } else {
+        // ...apply the .dark-theme class to override the default light styles
+        document.body.classList.toggle("dark-theme");
+        }
+    });
 });
